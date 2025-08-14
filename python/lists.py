71f751396312c8611199fa41b41c4c9bbcd5679c
Andrey <baykov.andrey@gmail.com>
2025-08-13 23:03:08 -0400

    Update login feature and step definitions; add new step for environment navigation and introduce utility Python scripts for basics

---
+++ python/lists.py
@@ -0,0 +1,31 @@
+# Lists
+# values = [7, "abc", 9, True, 2]
+# #         0    1    2   3    4
+# #         -5  -4   -3  -2   -1
+# print(values[-6])
+# print(type(values[-6]))
+#
+# name = "Al ice"
+# print(name[:3])
+
+# tuples
+# coordinates = (7, "abc", 9, True, 2)
+# print(coordinates[1])
+# print(type(coordinates[1]))
+
+# dict
+# student = {"age": 20, "id": "S12345", "name": "Alice"}
+# student["name"] = "Bob"
+# student["address"] = "New York"
+#
+# print(student)
+# print(type(student))
+
+# set
+# values = {1, 2, 3, 3, 4, 4, 5}
+# print(values)
+# print(type(values))
+
+# values = list(set([1, 2, 3, 4, "apple", "banana", "apple"]))
+# print(values)
+# print(type(values))
---
+++ python/variables.py
@@ -0,0 +1,28 @@
+# age = 12
+# size = 12.0
+# name = "Alice"
+# last_name = 'Smith'
+# middle_name = 'Alice "Red" Smith'
+# middle_name2 = "Alice 'Red' Smith"
+# middle_name3 = 'Alice \'Red\' Smith'
+# middle_name4 = "Alice \"Red\" Smith"
+# next_line = "Alice \n'Red' \nSmith"
+#
+# number_in_quotes = "12"
+# number_in_quotes2 = '12.1'
+#
+# print(number_in_quotes2)
+# print(type(number_in_quotes2))
+
+value_1 = int(input("Enter value 1: "))
+value_2 = int(input("Enter value 2: "))
+summ = value_1 + value_2
+print(summ)
+print(type(summ))
+
+
+a = True
+b = False
+
+print(a)
+print(type(a))
