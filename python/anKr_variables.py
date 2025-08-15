d47f6b49f5d457a07634dab8bd3a26bd34da6d6c
AnKrylo <akrylova09876@gmail.com>
2025-08-14 20:53:08 -0400

    Add anKr_variables.py with variable assignments and print statements

---
+++ python/anKr_variables.py
@@ -0,0 +1,28 @@
+#age = 6.2
+#name = "Charlik"
+
+#print(age)
+#print(type(age))
+
+#age = 6.2
+#name = "Charlik"
+
+#print(age, name)
+#print(type(age), type(name))
+# Variables can be reassigned
+
+age = 6.2
+name = "Charlik"
+size = 1.75
+last_name = 'Smith'
+middle_name = 'Charlik "Red" Smith'
+middle_name2 = "Charlik 'Red' Smith"
+middle_name3 = 'Charlik \'Red\' Smith'
+middle_name4 = "Charlik \"Red\" Smith"
+next_line = 'Charlik \n"Red" \n Smith'
+
+number_in_quotes = "12"
+number_in_quotes2 = '21.5'
+
+print(number_in_quotes2)
+print(type(number_in_quotes2))
