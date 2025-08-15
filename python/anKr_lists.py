aa32a31e0688de579fb83a5005ac1b6ddb1a580e
AnKrylo <akrylova09876@gmail.com>
2025-08-15 00:54:13 -0400

    Add examples and type checks for list and string operations in anKr_lists.py

--- python/anKr_lists.py
+++ python/anKr_lists.py
@@ -2,8 +2,37 @@
 # 1 List
 #value = [1, 2, 3, 4, 5]
 
-values = ["abcd", 2, 3.5, 8, True]
-print(values)
+# values = ["abcd", 2, 3.5, 8, True, [ 1, 2, 3]]
+# print(values)
+# print(type(values))
 
+# values = [ 7, "abc", 9, True, 2]
+# #          0    1    2   3    4
+# print(values[0])
+# print(type(values[0]))
 
+# values = [ 7, "abc", 9, True, 2]
+# #          0    1    2   3    4
+# print(values[5])
+# print(type(values[5]))
+
+# values = [ 7, "abc", 9, True, 2]
+# #         -5   -4   -3   -2   -1
+# print(values[-4])
+# print(type(values[-4]))
+
+# name = "Alice"
+# print(name [-2])
+
+# name = "Alice"
+# print(name [2:])
+
+# name = "Alice"
+# print(name [1:3])
+
+# name = "Alice"
+# print(name [:3])
+
+# name = "Al ice"
+# print(name [:5])
 
