7d8a3b1e53c460a0b817301273086e39252ffe05
AnKrylo <akrylova09876@gmail.com>
2025-08-16 17:12:24 -0400

    Update data

--- python/anKr_calc_functions.py
+++ python/anKr_calc_functions.py
@@ -1,17 +1,39 @@
 # Calculator
-def summary(num1, num2):
-    print(f"Sum: {num1 + num2}")
-    print(f"Diff: {num1 - num2}")
-    print(f"Product: {num1 * num2}")
-    print(f"Product: {num1 / num2}")
-a = 6
-b = 2
-summary(a,b)
+# def summary(num1, num2):
+#     print(f"Sum: {num1 + num2}")
+#     print(f"Diff: {num1 - num2}")
+#     print(f"Product: {num1 * num2}")
+#     print(f"Product: {num1 / num2}")
+# a = 6
+# b = 2
+# summary(a,b)
+#
+# a = 30
+# b = 15
+# summary(a,b)
+#
+# a = 8
+# b = 10
+# summary(a,b)
+from python.AD_Lists_Calculator import multiply
 
-a = 30
-b = 15
-summary(a,b)
 
-a = 8
-b = 10
-summary(a,b)
+# Calculater 1
+
+def sum(x, y):
+    return x + y
+x=6
+y=2
+print(f'x={x}', f'y={y}', f'sum = {sum(x, y)}')
+
+# def sub(x, y):
+#     return x - y
+# print(f'x={x}', f'y={y}', f'sub = {sub(x, y)}')
+#
+# def mul(x, y):
+#     return x * y
+# print(f'x={x}', f'y={y}', f'multiply = {multiply(x, y)}')
+
+# def div(x, y):
+#     return x / y
+# print(f'x={x}', f'y={y}', f'div = {div(x, y)}')
