6240fceb8c9b7edd8f02db5e4f8f112b60e0b36a
AnKrylo <akrylova09876@gmail.com>
2025-08-16 14:14:13 -0400

    Add examples for tuples, dictionaries, and sets in anKr_lists.py

---
+++ python/anKr_calc_functions.py
@@ -0,0 +1,12 @@
+# Calculator
+def summary(num1, num2):
+    print(f"Sum: {num1 + num2}")
+    print(f"Diff: {num1 - num2}")
+    print(f"Product: {num1 * num2}")
+    print(f"Product: {num1 / num2}")
+a = 6
+b = 2
+
+a = 30
+b = 15
+summary(a,b)
