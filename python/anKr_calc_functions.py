bac30055d470e3c47a15c63bc15067f65fdc14a9
AnKrylo <akrylova09876@gmail.com>
2025-08-16 17:21:40 -0400

    Update data

--- python/anKr_calc_functions.py
+++ python/anKr_calc_functions.py
@@ -1,24 +1,22 @@
-# Calculator
-# def summary(num1, num2):
-#     print(f"Sum: {num1 + num2}")
-#     print(f"Diff: {num1 - num2}")
-#     print(f"Product: {num1 * num2}")
-#     print(f"Product: {num1 / num2}")
-# a = 6
-# b = 2
-# summary(a,b)
-#
-# a = 30
-# b = 15
-# summary(a,b)
-#
-# a = 8
-# b = 10
-# summary(a,b)
-from python.AD_Lists_Calculator import multiply
+# Calculator 1
+def summary(num1, num2):
+    print(f"Sum: {num1 + num2}")
+    print(f"Diff: {num1 - num2}")
+    print(f"Product: {num1 * num2}")
+    print(f"Product: {num1 / num2}")
+a = 6
+b = 2
+summary(a,b)
 
+a = 30
+b = 15
+summary(a,b)
 
-# Calculater 1
+a = 8
+b = 10
+summary(a,b)
+
+# Calculater 2
 
 def sum(x, y):
     return x + y
@@ -26,14 +24,14 @@ x=6
 y=2
 print(f'x={x}', f'y={y}', f'sum = {sum(x, y)}')
 
-# def sub(x, y):
-#     return x - y
-# print(f'x={x}', f'y={y}', f'sub = {sub(x, y)}')
-#
-# def mul(x, y):
-#     return x * y
-# print(f'x={x}', f'y={y}', f'multiply = {multiply(x, y)}')
+def sub(x, y):
+    return x - y
+print(f'x={x}', f'y={y}', f'sub = {sub(x, y)}')
 
-# def div(x, y):
-#     return x / y
-# print(f'x={x}', f'y={y}', f'div = {div(x, y)}')
+def mul(x, y):
+    return x * y
+print(f'x={x}', f'y={y}', f'mul = {mul(x, y)}')
+
+def div(x, y):
+    return x / y
+print(f'x={x}', f'y={y}', f'div = {div(x, y)}')
