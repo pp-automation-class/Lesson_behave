d76a07b345c5a86be8a6fad1fde6e4db452415e7
Gaiane-ned <gaiane.ned@gmail.com>
2025-08-17 00:44:18 -0400

    Values and variables as a beginning

---
+++ Gaiane_python/variables_Gaiane.py
@@ -0,0 +1,19 @@
+# a="6"
+# b="2"
+# c=a+b
+# print(c)
+# print(type(c))
+
+# value1 = float(input("Enter value1: "))
+# value2 = float(input("Enter value2: "))
+# summ = value1 + value2
+# print(summ)
+# print(type(summ))
+
+a = True
+b = False
+
+print(a)
+print(type(a))
+
+
