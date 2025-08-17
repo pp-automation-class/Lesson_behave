4906ee289ea2d899a8fa57ddc4cdc4d5ae97f878
Gaiane-ned <gaiane.ned@gmail.com>
2025-08-17 00:43:45 -0400

    A lot of functions added

---
+++ Gaiane_python/list.py
@@ -0,0 +1,25 @@
+# value_1=10
+# value_2=20
+# value_3=30
+# value_4=45
+
+# lists
+# values = [5, 3, "abc", 8, True, [4, 7, 3], 9]
+# print(values)
+# print(type(values))
+
+# values = [5, 3, "abc", 8, True, 9]
+# #         0  1    2    3    4   5
+# #        -6   -5  -4   -3   -2  -1
+# print(values[6])
+# print(type(values[2]))
+
+# name = "Alice"
+# print(name[-1])
+# print(name[2:])
+# print(name[:2])
+# print(name[:-1])
+
+coordinates = (10.0, "abc", 20.0, True)
+print(coordinates[-1])
+print(type(coordinates[-1]))
