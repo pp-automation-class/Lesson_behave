c2038567154cd1149d4f94e7cd79d2b56f8cf1e4
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-08-17 16:55:33 -0500

    Refactor kilograms/pounds conversion program to include loop and exit option

--- python/lstark_conversion.py
+++ python/lstark_conversion.py
@@ -14,26 +14,28 @@ elif type_of_conversion == 'k':
 
 else:
     print("Inavalid input, please enter 'm' or 'k'")
-
 print("End of program")
 print("\n")
 
 #Kilo/Pounds
 
 print("Helo! Kilograms to Pounds conversion program.")
-type_of_conversion = input("Enter 'k' for kilograms to pounds' or 'p' for pounds to kilograms:")
+while True:
+    type_of_conversion = input("Enter 'k' for kilograms to pounds' or 'p' for pounds to kilograms:")
 
-if type_of_conversion == 'k':
-    answer= float(input("Enter number of kilograms:"))
-    print(f"{answer} kilograms is {answer * 2.20462} pounds")
+    if type_of_conversion == 'k':
+        answer = float(input("Enter number of kilograms:"))
+        print(f"{answer} kilograms is {answer * 2.20462} pounds")
+        #print("\n")
 
-elif type_of_conversion == 'p':
-    answer = float(input("Enter number of pounds:"))
-    print(f"{answer} pounds is {answer/ 2.20462} kilograms")
-
-else:
-    print("Invalid input, please enter 'k' or 'p'")
-
-print("End of program")
+    elif type_of_conversion == 'p':
+        answer = float(input("Enter number of pounds:"))
+        print(f"{answer} pounds is {answer / 2.20462} kilograms")
+        #
+    elif type_of_conversion == 'stop':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input, please enter 'k' or 'p'")
 
 
