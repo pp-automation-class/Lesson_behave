abd29428e08f7253e63368aae25ba09c9c78a27f
Your Name <alena9tester@gmail.com>
2025-08-18 10:12:39 -0600

    Add Python scripts to demonstrate variable types and data structures

---
+++ python/Linda_value.py
@@ -0,0 +1,40 @@
+# LIST
+
+# Linda_list = ['Linda', 'John', 'Alice', 'Bob', 123, 4,True]
+# print(Linda_list)
+# print(type(Linda_list))
+#
+# print(Linda_list[2:])
+# print(Linda_list[2:4])
+#
+# Linda_list = Linda_list + ['Charlie', 'Eve']
+#
+# print(Linda_list)
+# print(type(Linda_list))
+
+
+
+# # Tuple
+# Linda_tuple = ('Linda', 'John', 'Alice', 'Bob', 123, 4, True)
+# print(Linda_tuple)
+# print(type(Linda_tuple))
+# print(Linda_tuple[2:])
+# print(Linda_tuple[2:4])
+
+# Set
+# Linda_set = {'Linda', 'John', 'Alice', 'Bob', 123, 4, True}
+#
+# Linda_set.update({1, 1,1,2,2,2,34,56,2,78,90,12,34,56,78,90})
+# print(Linda_set)
+# print(type(Linda_set))
+
+
+
+# Dictionary
+Linda_dict = {
+    'name': 'Linda',
+    'age': 37,
+    'city': 'Denver',
+    'is_student': False}
+print(Linda_dict)
+print(type(Linda_dict))
