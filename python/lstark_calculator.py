3825defc869a82f15f796fd510e80b6b4219b3a0
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-08-18 16:39:35 -0500

    wip

--- python/lstark_calculator.py
+++ python/lstark_calculator.py
@@ -1,1 +1,17 @@
 # HW by Laura Stark
+
+#Value1 = [0, 1,  2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#value_1 = float(input("Enter value 1: "))
+#value_2 = float(input("Enter value 2: "))
+
+print("Calculator")
+
+while True:
+    type_of_operation = input(Enter )
+
+
+
+print (int(input("Enter Value1 - number from 0 to 10")))
+
+print (f*{answer} )
