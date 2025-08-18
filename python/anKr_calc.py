a9957299e70503974a82dbd0a86d37e474bd537a
AnKrylo <akrylova09876@gmail.com>
2025-08-18 01:24:15 -0400

    Rename anKr_calc.py

---
+++ python/anKr_calc.py
@@ -0,0 +1,46 @@
+# Calculator 1
+from python.conditions import answer
+
+#
+# def summary(num1, num2):
+#     print(f"Sum: {num1 + num2}")
+#     print(f"Diff: {num1 - num2}")
+#     print(f"Product: {num1 * num2}")
+#     print(f"Product: {num1 / num2}")
+# a = 6
+# b = 2
+# summary(a,b)
+#
+# a = 30
+# b = 15
+# summary(a,b)
+#
+# a = 8
+# b = 10
+# summary(a,b)
+#
+# # Calculater 2
+#
+# def sum(x, y):
+#     return x + y
+# x=6
+# y=2
+# print(f'x={x}', f'y={y}', f'sum = {sum(x, y)}')
+#
+# def sub(x, y):
+#     return x - y
+# print(f'x={x}', f'y={y}', f'sub = {sub(x, y)}')
+#
+# def mul(x, y):
+#     return x * y
+# print(f'x={x}', f'y={y}', f'mul = {mul(x, y)}')
+#
+# def div(x, y):
+#     return x / y
+# print(f'x={x}', f'y={y}', f'div = {div(x, y)}')
+
+# Calculater 3 Miles to kilometers calculator
+
+print("Hello! Miles to kilometers calculator")
+answer = float(input("Enter a number of miles: "))
+print(f"{answer} miles is {answer*1.609} kilometers")
