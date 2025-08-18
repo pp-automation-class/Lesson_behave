3feeb86cf20b19fec1d5e0038d477c9747f84f1b
Your Name <alena9tester@gmail.com>
2025-08-18 13:37:07 -0600

    Add `Linda_functions.py` with example Python functions

---
+++ python/Linda_functions.py
@@ -0,0 +1,57 @@
+# 1. Function without parameters
+def say_goodbye():
+    print("Goodbye, world!")
+say_goodbye()
+
+
+
+def say_hello():
+    print("Hello, world!")
+
+say_hello()
+
+def tell_me_name():
+    print("My name is Google.")
+tell_me_name()
+
+def как_я_зае_устала():
+    print("Пора отдыхать!")
+как_я_зае_устала()
+
+def не_входить():
+    print("Не входить!")
+не_входить()
+
+# 2. Function with parameters
+
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Linda")
+greet("Santa")
+
+def write_your_name(name):
+    print(f"напишите ваше имя: {name}")
+write_your_name("Linda")
+write_your_name("Santa")
+
+def write_your_name(name):
+    print(f" {name}")
+write_your_name("Linda")
+write_your_name("Santa")
+
+# 3. Function with a return value
+
+def add(a, b):
+    return a + b
+result = add(5, 3)
+print(result)
+
+def grett(name):
+    return f"Hello, {name}!"
+greeting = grett("Linda")
+print(greeting)
+
+
+
+
+
