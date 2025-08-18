df34eedbd24a57e668be6e0b456cf4ebf88116ab
amithag0486 <gamitha.adiga@gmail.com>
2025-08-18 15:44:43 -0700

    Python Practice with the Fibonacci series

--- python/Amitha_PythonPractice.py
+++ python/Amitha_PythonPractice.py
@@ -1,7 +1,7 @@
 var = 12
 firstName = 'John'
 lastName = "Doe"
-
+# Basic calculator
 value1 = float(input("Enter a number: "))
 value2 = float(input("Enter another number: "))
 
@@ -25,6 +25,7 @@ diffrence(value1, value2)
 multiply(value1, value2)
 division(value1, value2)
 
+#Coversion of Km to Miles and vise versa
 type_converion = input("Type M for coverstion to miles or type K for coverstion to kilometers ")
 covertion = float(input("Enter a number: "))
 def conversion(covertion):
@@ -36,3 +37,25 @@ def conversion(covertion):
         print("type the correct coverstion type")
 
 conversion(covertion)
+
+#Printing a Fibonoci series till n
+
+n = int(input("Enter the number for fibnoccci series: "))
+
+def fibnoccci(firstNumber,secondNumber):
+    i = 1
+    fibSeries = [firstNumber, secondNumber]
+    while i <=n:
+        nextNumber = firstNumber + secondNumber
+       # fibSeries = [firstNumber, secondNumber]
+      #  print(f"Fibonnoccci = {firstNumber} , {secondNumber} , {nextNumber}")
+        firstNumber = secondNumber
+        secondNumber = nextNumber
+        fibSeries.append(nextNumber)
+        i = i + 1
+    print(fibSeries)
+fibnoccci(0,1)
+
+
+
+
