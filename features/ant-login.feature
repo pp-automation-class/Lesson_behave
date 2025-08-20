29fed1651141b3158184526c1f3c485fc63d49f3
Antik-Bond <antikbond@gmail.com>
2025-08-20 18:40:09 -0400

    Add custom step definition "ANT I login as "tester""

--- features/ant-login.feature
+++ features/ant-login.feature
@@ -4,15 +4,11 @@ Feature: LinkMyGear Application Login
     Given I navigate to "https://dev.linkmygear.com"
     And I verify element "//h5" contains text "Account"
 
-  Scenario: Successful user login with valid credentials
-    When I fill "anton.bondarenko.test@gmail.com" in element "//input[@name='username']"
-    And I fill "123459" in element "//input[@name='password']"
-    And I click on "//button[text()=' Login ']"
-    Then I verify element "//h3[contains(text(), 'My devices ')]" exists
+    Scenario: Successful user login with valid credentials
+    When ANT I login as "tester"
+    Then I verify element "//h3[contains(text(), 'My devices')]" exists
 
     Scenario: Unsuccessful user login with invalid credentials
-    When I fill "anton.bondarenko.test@gmail.com" in element "//input[@name='username']"
-    When I fill "1a2b3c4D!" in element "//input[@name='password']"
-    And I click on "//button[text()=' Login ']"
+    When ANT I login as "admin"
     Then I verify element "//h5" contains text "Account"
     And I verify element "//p[text()='Sorry, unrecognized username or password.']" exists
--- steps/ant-steps.py
+++ steps/ant-steps.py
@@ -1,4 +1,39 @@
-
 from behave import step
 
 
+def fill_text_in_element(context, text, xpath):
+        if text != 'Skip':
+        context.page.fill_element(xpath, text)
+
+def click_on_element(context, xpath):
+        context.page.click_element(xpath)
+
+@step('ANT I login as "{user_type}"')
+def ant_login_as_tester(context, user_type):
+    """
+    Args:
+        context (behave.runner.Context):
+    """
+
+    if user_type == 'tester':
+        user_name = 'anton.bondarenko.test@gmail.com'
+        password = '123459'
+    elif user_type == 'admin':
+        user_name = 'admin@gmail.com'
+        password = '7654321'
+    else:
+        user_name = 'anton.bondarenko.test@gmail.com'
+        password = '123xxx'
+
+    # fill tester username
+    user_name_xpath = "//input[@name='username']"
+    fill_text_in_element(context, user_name, user_name_xpath)
+    # fill tester password
+    password_xpath = "//input[@name='password']"
+    fill_text_in_element(context, password, password_xpath)
+    # click login button
+    login_button_xpath = "//button[text()=' Login ']"
+    click_on_element(context, login_button_xpath)
+
+
+    raise NotImplementedError(u'STEP: When ANT I login as "tester"')
