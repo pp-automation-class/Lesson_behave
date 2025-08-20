1393083cc7f285aec6b029f2171b259d7aff756f
aadok <alex.dok10@gmail.com>
2025-08-20 16:22:16 +0300

    Lesson_11_Homework

--- python/AD_loops.py
+++ python/AD_loops.py
@@ -1,10 +1,12 @@
 cars = ['Ford', 'BMW', 'Volvo','Audi', 'Mercedes', 'Tesla']
 for i in cars:
     print(i)
+print("\n")
 
 name='Alice'
 for i in name:
     print(i)
+print("\n")
 
 #for i in range(5, 11):
     #print(i)
@@ -12,18 +14,39 @@ for i in name:
 
 for _ in range(2):
     print("Hello")
+print("\n")
 
-for i in range(10):
+for i in range(7):
     if i == 5:
         continue
     if i!=3:
         print(i)
     else:
         print("skipping")
+print("\n")
+
+i=0
+while i<4:
+    print(i)
+    i=i+1
+print("\n")
+
+i=0
+result=True
+while result:
+    print(i)
+    i=i+1
+    if i==5:
+        result=False
+print("\n")
+
+fruits = [["apple", "banana", "cherry"], ["mango", "orange"]]
+for fruit in fruits:
+    for f in fruit:
+        print(f)
 
 
 
 
-    #if i==4:
-     #   continue
-     #  print(i)
+
+print("End of program")
