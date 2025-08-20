2cf3aecabcc8ca5b465f3852350e90be07c488c1
aadok <alex.dok10@gmail.com>
2025-08-20 15:47:04 +0300

    Lesson_11_Homework

---
+++ python/AD_loops.py
@@ -0,0 +1,29 @@
+cars = ['Ford', 'BMW', 'Volvo','Audi', 'Mercedes', 'Tesla']
+for i in cars:
+    print(i)
+
+name='Alice'
+for i in name:
+    print(i)
+
+#for i in range(5, 11):
+    #print(i)
+    #print ('Hello')
+
+for _ in range(2):
+    print("Hello")
+
+for i in range(10):
+    if i == 5:
+        continue
+    if i!=3:
+        print(i)
+    else:
+        print("skipping")
+
+
+
+
+    #if i==4:
+     #   continue
+     #  print(i)
