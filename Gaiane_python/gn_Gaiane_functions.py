ac3bd4dc5ad951aa72dc3ec158b5b7853d20f722
Gaiane-ned <gaiane.ned@gmail.com>
2025-08-20 15:54:03 -0400

    Added content of list.py file and functions: summ, diff, product, quotient

--- Gaiane_python/gn_Gaiane_functions.py
+++ Gaiane_python/gn_Gaiane_functions.py
@@ -1,3 +1,55 @@
+# value_1=10
+# value_2=20
+# value_3=30
+# value_4=45
+
+# lists
+# values = [5, 3, "abc", 8, True, [4, 7, 3], 9]
+# print(values)
+# print(type(values))
+
+# values = [5, 3, "abc", 8, True, 9]
+# #         0  1    2    3    4   5
+# #        -6   -5  -4   -3   -2  -1
+# print(values[6])
+# print(type(values[2]))
+
+# name = "Alice"
+# print(name[-1])
+# print(name[2:])
+# print(name[:2])
+# print(name[:-1])
+
+# tuples
+# coordinates = (10.0, "abc", 20.0, True)
+# print(coordinates[-1])
+# print(type(coordinates[-1]))
+
+# dict
+student = ["Elza", 21, "KM589746"]
+# student[0] = "Bob"
+# student.append("kodos1")
+# student.append("kodos2")
+# student.append("kodos3")
+# student.append("kodos4")
+# student.append("kodos5")
+# student = {"name": "Elza", "age": 21, "id": "KM589746"}
+# student["name"] = "Bob"
+# student["address"] = "Chicago"
+# print(student["name"])
+# print(student["age"])
+# print(student)
+# print(type(student))
+#
+# # set
+# values = {1, 2, 3, 4, 4, 5}
+# print(values)
+# print(type(values))
+
+values = list(set([1,2,3,4, "apple", "banana", "apple"]))
+print(values)
+print(type(values))
+
 # a="6"
 # b="2"
 # c=a+b
@@ -10,27 +62,47 @@
 # print(summ)
 # print(type(summ))
 
-a = True
-b = False
+# a = True
+# b = False
+#
+# print(a)
+# print(type(a))
+#
+# a="15"
+# b="4"
+# c=a+b
+# print(c)
+# print(type(c))
 
-print(a)
-print(type(a))
+# value1 = float(input("Enter value1: "))
+# value2 = float(input("Enter value2: "))
+# summ = value1 + value2
+# print(summ)
+# print(type(summ))
+#
+# value1 = int(input("Enter value1: "))
+# value2 = int(input("Enter value2: "))
+# summ = value1 + value2
+# print(summ)
+# print(type(summ))
+#
+# value1 = float(input("Enter value1: "))
+# value2 = float(input("Enter value2: "))
+# diff = value1 - value2
+# print(diff)
+# print(type(diff))
+#
+# value1 = float(input("Enter value1: "))
+# value2 = float(input("Enter value2: "))
+# product = value1 * value2
+# print(product)
+# print(type(product))
+#
+# value1 = float(input("Enter value1: "))
+# value2 = float(input("Enter value2: "))
+# quotient = value1 / value2
+# print(quotient)
+# print(type(quotient))
 
-a="15"
-b="4"
-c=a+b
-print(c)
-print(type(c))
 
-value1 = float(input("Enter value1: "))
-value2 = float(input("Enter value2: "))
-summ = value1 + value2
-print(summ)
-print(type(summ))
-
-value1 = int(input("Enter value1: "))
-value2 = int(input("Enter value2: "))
-summ = value1 + value2
-print(summ)
-print(type(summ))
 
