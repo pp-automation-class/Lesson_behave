e3b22d27c25f13f2d6567532dd49da7ce1b7bdd3
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-08-20 20:55:43 -0500

    wip

---
+++ steps/lstark_steps.py
@@ -0,0 +1,35 @@
+
+from behave import step
+
+from steps.steps import click_on_element
+
+
+@step('lstark I login as "{user_type}"')
+def lstark_login_as_user(context, user_type):
+    """
+    Args:
+        user_type: Type of user to login as
+        context (behave.runner.Context):
+    """
+    if user_type == 'user':
+        user_name = 'lauravstesting53@gmail.com'
+        password = 'T8st8ng38!'
+    elif user_type == 'admin':
+        user_name = 'pcs.automationclass@gmail.com'
+        password = '1234567'
+    else:
+        user_name = 'pcs.automationclass@gmail.com'
+        password = 'xxxxxx'
+
+    #fill username
+    user_name_xpath = "//input[@name='username']"
+    fill_text_in_element(context, user_name, user_name_xpath)
+    #fill password
+    password_xpath = "//input[@name='password']"
+    fill_text_in_element(context, password, password_xpath)
+    #Click button
+    login_button_xpath = "//button[text()=' Login ']"
+    click_on_element(context, login_button_xpath)
+
+
+
