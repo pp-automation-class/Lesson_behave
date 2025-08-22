f5e39ff38fcb4ce4f22ea7d62b2cb7ac20a60f21
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-08-22 16:46:09 -0500

    Created step - lstark I log in as {user_type} with if statement

--- steps/lstark_steps.py
+++ steps/lstark_steps.py
@@ -1,9 +1,6 @@
 
 from behave import step
 
-from steps.steps import click_on_element
-
-
 @step('lstark I login as "{user_type}"')
 def lstark_login_as_user(context, user_type):
     """
@@ -21,15 +18,22 @@ def lstark_login_as_user(context, user_type):
         user_name = 'pcs.automationclass@gmail.com'
         password = 'xxxxxx'
 
-    #fill username
+
+    # fill username
+    def fill_text_in_element(context, text, xpath):
+        if text != 'Skip':
+            context.page.fill_element(xpath, text)
+
     user_name_xpath = "//input[@name='username']"
     fill_text_in_element(context, user_name, user_name_xpath)
-    #fill password
+
+    # fill password
     password_xpath = "//input[@name='password']"
     fill_text_in_element(context, password, password_xpath)
-    #Click button
+
+    # Click button
+    def click_on_element(context, xpath):
+        context.page.click_element(xpath)
+
     login_button_xpath = "//button[text()=' Login ']"
     click_on_element(context, login_button_xpath)
-
-
-
