d03178156a0c72ce64f608e534573609a6582299
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-08-22 17:03:23 -0500

    I commented out "if statement" and #fill_text_in_element for user_name_xpath and password_xpath) and added user_credentials variables using Tuple array.

--- steps/lstark_steps.py
+++ steps/lstark_steps.py
@@ -8,15 +8,20 @@ def lstark_login_as_user(context, user_type):
         user_type: Type of user to login as
         context (behave.runner.Context):
     """
-    if user_type == 'user':
-        user_name = 'lauravstesting53@gmail.com'
-        password = 'T8st8ng38!'
-    elif user_type == 'admin':
-        user_name = 'pcs.automationclass@gmail.com'
-        password = '1234567'
-    else:
-        user_name = 'pcs.automationclass@gmail.com'
-        password = 'xxxxxx'
+    # if user_type == 'user':
+    #     user_name = 'lauravstesting53@gmail.com'
+    #     password = 'T8st8ng38!'
+    # elif user_type == 'admin':
+    #     user_name = 'pcs.automationclass@gmail.com'
+    #     password = '1234567'
+    # else:
+    #     user_name = 'pcs.automationclass@gmail.com'
+    #     password = 'xxxxxx'
+
+    user_credentials = {
+        "user": ("lauravstesting53@gmail.com", "T8st8ng38!"),
+        "admin": ("pcs.automationclass@gmail.com", "1234567")
+    }
 
 
     # fill username
@@ -25,11 +30,13 @@ def lstark_login_as_user(context, user_type):
             context.page.fill_element(xpath, text)
 
     user_name_xpath = "//input[@name='username']"
-    fill_text_in_element(context, user_name, user_name_xpath)
+    #fill_text_in_element(context, user_name, user_name_xpath)
+    fill_text_in_element(context,user_credentials[user_type][0], user_name_xpath)
 
     # fill password
     password_xpath = "//input[@name='password']"
-    fill_text_in_element(context, password, password_xpath)
+    #fill_text_in_element(context, password, password_xpath)
+    fill_text_in_element(context, user_credentials[user_type][1], password_xpath)
 
     # Click button
     def click_on_element(context, xpath):
