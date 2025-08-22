e5eff5e469c73a6991043d8ea33f774e5b2de3d6
AnKrylo <akrylova09876@gmail.com>
2025-08-22 00:54:31 -0400

    Work in progress

--- steps/anKr_steps.py
+++ steps/anKr_steps.py
@@ -8,30 +8,26 @@ def ankr_login_as_user(context, user_type):
         user_type: Type of user to login as
         context (behave.runner.Context):
     """
-    # if user_type == 'user':
-    #     user_name = 'akr.autotest@gmail.com'
-    #     password = '12345'
-    # elif user_type == 'admin':
-    #     user_name = 'admin@gmail.com'
-    #     password = 'admin1234'
-    # else:
-    #     user_name = 'autotest@gmail.com'
-    #     password = '13579'
-
-
-    user_credentials = {
-        "user": ("akr.autotest@gmail.com", "12345"),
-        "admin": ("akr.autotest@gmail.com", "13579")
-    }
+    if user_type == 'user':
+        user_name = 'akr.autotest@gmail.com'
+        password = '12345'
+    elif user_type == 'admin':
+        user_name = 'admin@gmail.com'
+        password = 'admin1234'
+    else:
+        user_name = 'autotest@gmail.com'
+        password = '13579'
 
     # fill username
+    def fill_text_in_element(context, text, xpath):
+        if text != 'Skip':
+            context.page.fill_element(xpath, text)
+
     user_name_xpath = "//input[@name='username']"
-    # fill_text_in_element(context, user_name, user_name_xpath)
-    fill_text_in_element(context, user_credentials[user_type][0], user_name_xpath)
+    fill_text_in_element(context, user_name, user_name_xpath)
     # fill password
     password_xpath = "//input[@name='password']"
-    # fill_text_in_element(context, password, password_xpath)
-    fill_text_in_element(context, user_credentials[user_type][1], password_xpath)
+    fill_text_in_element(context, password, password_xpath)
     # Click login button
 
     def click_on_element(context, xpath):
