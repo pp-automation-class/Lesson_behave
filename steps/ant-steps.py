3ae4a612c58d1624539a28c569ed577de705f3b1
Antik-Bond <antikbond@gmail.com>
2025-08-24 14:45:48 -0400

    Met DRY rules - Call function from steps.py

--- steps/ant-steps.py
+++ steps/ant-steps.py
@@ -1,4 +1,5 @@
 from behave import step
+from steps import fill_text_in_element, click_on_element, wait_for_element,verify_element_exists
 
 
 @step('ANT I login as "{user_type}"')
@@ -20,10 +21,6 @@ def ant_login_as_tester(context, user_type):
         password = '123xxx'
 
     # fill tester username
-    def fill_text_in_element(context, text, xpath):
-        if text != 'Skip':
-            context.page.fill_element(xpath, text)
-
     user_name_xpath = "//input[@name='username']"
     fill_text_in_element(context, user_name, user_name_xpath)
     # fill tester password
@@ -31,26 +28,17 @@ def ant_login_as_tester(context, user_type):
     fill_text_in_element(context, password, password_xpath)
     # click login button
 
-    def click_on_element(context, xpath):
-        context.page.click_element(xpath)
-
     login_button_xpath = "//button[text()=' Login ']"
     click_on_element(context, login_button_xpath)
 
 
 @step('ANT I add new device "{device_name}"')
 def ant_add_new_device(context, device_name):
-    # click add device button
-    def click_on_element(context, xpath):
-        context.page.click_element(xpath)
     element_xpath = "//button[.//span[text()='Add new device']]"
     click_on_element(context, element_xpath)
 
     # wait for element visible
     element_xpath = "//h3[@class='modal-title' and text()='Add device']"
-
-    def wait_for_element(context, xpath):
-        context.page.wait_for_element(xpath)
     wait_for_element(context, element_xpath)
 
     # select devise type
@@ -61,10 +49,6 @@ def ant_add_new_device(context, device_name):
     click_on_element(context, element_xpath)
 
     # fill device name
-    def fill_text_in_element(context, text, xpath):
-        if text != 'Skip':
-            context.page.fill_element(xpath, text)
-
     element_xpath = "//input[@class='el-input__inner']"
     fill_text_in_element(context, device_name, element_xpath)
 
@@ -76,16 +60,11 @@ def ant_add_new_device(context, device_name):
 @step('ANT I delete "{device_name}"')
 def ant_delete_device(context, device_name):
     # click delete button
-    def click_on_element(context, xpath):
-        context.page.click_element(xpath)
     element_xpath = f"//tr[@class='el-table__row' and .//span[text()='{device_name}']]//button[text()=' Delete ']"
     click_on_element(context, element_xpath)
 
     # wait for element visible
     element_xpath = "//h3[@class='modal-title' and text()='Delete device']"
-
-    def wait_for_element(context, xpath):
-        context.page.wait_for_element(xpath)
     wait_for_element(context, element_xpath)
 
     # confirm delete
@@ -93,8 +72,5 @@ def ant_delete_device(context, device_name):
     click_on_element(context, element_xpath)
 
     # verify device delete confirmation message
-    def verify_element_exists(context, xpath):
-        context.page.verify_element_exists(xpath)
-
     element_xpath = "//*[contains(@class, 'el-message__content') and contains(text(), 'Device deleted')]"
     verify_element_exists(context, element_xpath)
