01eff1ed1b745564762086a583459156dd5d1b53
Antik-Bond <antikbond@gmail.com>
2025-08-24 15:40:23 -0400

    Xpaths in classes: AntLoginPage, AntDevicePage

--- steps/ant-steps.py
+++ steps/ant-steps.py
@@ -2,6 +2,25 @@ from behave import step
 from steps import fill_text_in_element, click_on_element, wait_for_element,verify_element_exists
 
 
+class AntLoginPage:
+
+    def __init__(self):
+        self.user_name = "//input[@name='username']"
+        self.password = "//input[@name='password']"
+        self.login_button = "//button[text()=' Login ']"
+
+class AntDevicePage:
+
+    def __init__(self):
+        self.add_new_device = "//button[.//span[text()='Add new device']]"
+        self.add_device = "//h3[@class='modal-title' and text()='Add device']"
+        self.device_type = "//label[text()='Device type']"
+        self.device_airguard = "//li[.//span[text()='Airguard other']]"
+        self.device_name_field = "//input[@class='el-input__inner']"
+        self.add_new_device_button =  "//div[@class='form-submit']/button[.//span[text()='Add new device']]"
+
+
+
 @step('ANT I login as "{user_type}"')
 def ant_login_as_tester(context, user_type):
     """
@@ -21,39 +40,38 @@ def ant_login_as_tester(context, user_type):
         password = '123xxx'
 
     # fill tester username
-    user_name_xpath = "//input[@name='username']"
+    user_name_xpath = AntLoginPage().user_name
     fill_text_in_element(context, user_name, user_name_xpath)
     # fill tester password
-    password_xpath = "//input[@name='password']"
+    password_xpath = AntLoginPage().password
     fill_text_in_element(context, password, password_xpath)
     # click login button
-
-    login_button_xpath = "//button[text()=' Login ']"
+    login_button_xpath = AntLoginPage().login_button
     click_on_element(context, login_button_xpath)
 
 
 @step('ANT I add new device "{device_name}"')
 def ant_add_new_device(context, device_name):
-    element_xpath = "//button[.//span[text()='Add new device']]"
+    element_xpath = AntDevicePage().add_new_device
     click_on_element(context, element_xpath)
 
     # wait for element visible
-    element_xpath = "//h3[@class='modal-title' and text()='Add device']"
+    element_xpath = AntDevicePage().add_device
     wait_for_element(context, element_xpath)
 
     # select devise type
-    element_xpath = "//label[text()='Device type']"
+    element_xpath = AntDevicePage().device_type
     click_on_element(context, element_xpath)
 
-    element_xpath = "//li[.//span[text()='Airguard other']]"
+    element_xpath = AntDevicePage().device_airguard
     click_on_element(context, element_xpath)
 
     # fill device name
-    element_xpath = "//input[@class='el-input__inner']"
+    element_xpath = AntDevicePage().device_name_field
     fill_text_in_element(context, device_name, element_xpath)
 
     # click add new device button
-    element_xpath = "//div[@class='form-submit']/button[.//span[text()='Add new device']]"
+    element_xpath = AntDevicePage().add_new_device_button
     click_on_element(context, element_xpath)
 
 
