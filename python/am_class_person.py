b1e2b8b40173fa363355e3b413a64494cb907823
alexdamir <aamalyy@gmail.com>
2025-08-24 20:02:58 -0400

    Refactor `Person` class in `am_class_person.py` by introducing `PersonData` and `PersonDataCore` for modular data handling; add unique identifier and extend functionality with marriage tracking.

--- python/am_class_person.py
+++ python/am_class_person.py
@@ -1,116 +1,64 @@
 import datetime
+import uuid
 
 
-class Person:
-    marriages = []
-    children = []
-    pets = []
-    jobs = []
-    addresses = []
-    phone_numbers = []
-    emails = []
-    social_media = []
-    relatives = []
-    medical_conditions = []
-    allergies = []
-    medications = []
-    hobbies = []
-    skills = []
-    languages = []
-    education = []
-    certifications = []
-    memberships = []
-    notes = []
-    documents = []
-    photos = []
-    videos = []
-    audio_recordings = []
-    websites = []
-    social_security_number = None
-    driver_license_number = None
-    passport_number = None
-    tax_identification_number = None
-    credit_card_number = None
-    bank_account_number = None
-    insurance_policy_number = None
-    emergency_contacts = []
-    favorite_colors = []
-    favorite_foods = []
-    favorite_movies = []
-    favorite_books = []
-    favorite_authors = []
-    favorite_music = []
-    favorite_places = []
-    favorite_quotes = []
-    favorite_sports = []
-    favorite_activities = []
-    favorite_holidays = []
-    favorite_seasons = []
-    favorite_animals = []
-    favorite_flowers = []
-    favorite_countries = []
-    favorite_cities = []
-    favorite_restaurants = []
-    favorite_shops = []
-    favorite_websites = []
-    favorite_apps = []
-    favorite_games = []
-    favorite_tv_shows = []
-    favorite_podcasts = []
-    favorite_youtubers = []
-    favorite_influencers = []
-    favorite_celebrities = []
-    favorite_historical_figures = []
-    favorite_fictional_characters = []
-    favorite_heroes = []
-    favorite_villains = []
-    favorite_superheroes = []
-    favorite_supervillains = []
-    favorite_mythological_figures = []
-    favorite_folklore_figures = []
-    favorite_legends = []
-    favorite_fables = []
-    favorite_fairy_tales = []
-    favorite_nursery_rhymes = []
-    favorite_jokes = []
-    favorite_memes = []
-    favorite_sayings = []
-    favorite_idioms = []
-    favorite_proverbs = []
-    favorite_catchphrases = []
-    favorite_slogans = []
-    favorite_mottos = []
-    favorite_mantras = []
-    favorite_affirmations = []
-    favorite_prayers = []
-    favorite_quotes_from_books = []
-    favorite_quotes_from_movies = []
-    favorite_quotes_from_tv_shows = []
-    favorite_quotes_from_songs = []
-    favorite_quotes_from_speeches = []
-    favorite_quotes_from_historical_figures = []
-    favorite_quotes_from_fictional_characters = []
-    favorite_quotes_from_heroes = []
-    favorite_quotes_from_villains = []
-    favorite_quotes_from_superheroes = []
-    favorite_quotes_from_supervillains = []
-    favorite_quotes_from_mythological_figures = []
-    favorite_quotes_from_folklore_figures = []
-    favorite_quotes_from_legends = []
-    favorite_quotes_from_fables = []
-    favorite_quotes_from_fairy_tales = []
-    favorite_quotes_from_nursery_rhymes = []
-    favorite_quotes_from_jokes = []
-    favorite_quotes_from_memes = []
-    favorite_quotes_from_sayings = []
+class PersonData:
+    data = {
+        "marriages": [],
+        "children": [],
+        "pets": [],
+        "jobs": [],
+        "addresses": [],
+        "phone_numbers": [],
+        "emails": [],
+        "social_media": [],
+        "relatives": [],
+        "medical_conditions": [],
+        "allergies": [],
+        "medications": [],
+        "hobbies": [],
+        "skills": [],
+        "languages": [],
+        "education": [],
+        "certifications": [],
+        "memberships": [],
+        "notes": [],
+        "documents": [],
+        "photos": [],
+        "videos": [],
+        "social_security_number": None,
+        "driver_license_number": None,
+        "passport_number": None,
+        "tax_identification_number": None,
+        "credit_cards": [],
+        "bank_account_numbers": [],
+        "insurance_policy_numbers": [],
+        "vehicle_information": [],
+        "emergency_contacts": [],
+        "favorite_foods": [],
+        "favorite_movies": [],
+        "favorite_books": [],
+        "favorite_authors": [],
+        "favorite_music": [],
+        "favorite_games": [],
+    }
+
+
+class PersonDataCore(PersonData):
+    def marriage(self, spouse_id, date):
+        self.data["marriages"].append({"spouse": spouse_id, "date": date})
+
+
+class PersonCore(PersonDataCore):
 
     def __init__(self, first_name: str, last_name: str, birthday=None):
         self.birthday = birthday
         self.first_name = first_name
         self.last_name = last_name
+        self.id = uuid.uuid4()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, born on {self.birthday}"
+        return f"{self.first_name} {self.last_name}, born on {self.birthday}, Identifier in matrix: {self.id}"
 
     @property
     def age(self):
@@ -126,13 +74,29 @@ class Person:
             return None
 
 
+class Person(PersonCore):
+    pass
+
+
 p1 = Person("John", "Smith", "8/1/1990")
 
-print(p1.first_name)
-print(p1.last_name)
+print(f"first name: {p1.first_name}")
+print(f"last name: {p1.last_name}")
 
-print(p1.birthday)
+print(f"birthday: {p1.birthday}")
 
-print(p1.age)
+print(f"age: {p1.age}")
 
-print(p1)
+print(f"id: {p1.id}")
+
+print(f"object: {p1}")
+
+p2 = Person("Alisa", "North", "4/11/1998")
+
+print(f"object: {p2}")
+
+print(f"marriages: {p1.data['marriages']}")
+
+p1.marriage(p2.id, "6/15/2015")
+
+print(f"marriages: {p1.data['marriages']}")
