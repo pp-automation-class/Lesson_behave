ca5f74a65011e0c5fd1f8487d55aea4b2305e566
amithag0486 <gamitha.adiga@gmail.com>
2025-08-27 18:59:20 -0700

    classes practice

---
+++ python/amitha_classes.py
@@ -0,0 +1,33 @@
+from python.Amitha_PythonPractice import firstName
+
+
+class Amitha_cars:
+    def __init__(self, model, make, year, condition = bool):
+        self.model = model
+        self.make = make
+        self.year = year
+        self.condition = condition
+
+    def __str__(self):
+        return f'{self.model} {self.make} {self.year} {self.condition}'
+
+c1 = Amitha_cars("Ford", "Mustang", 1999, True)
+print(c1)
+
+class Amitha_person:
+    def __init__(self, firstname, lastname, age, marital_status=None, address=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.marital_status = marital_status if marital_status else "single"
+        self.address = address
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}, Age: {self.age}, Marital Status: {self.marital_status}, Address: {self.address}'
+
+
+# Creating an object
+p1 = Amitha_person('Amitha', 'Ganesh', 30, 'single', 'Dublin, CA')
+
+print(p1)
+
