26dfcc7a0796ba7d110155bc85f5cd1ca6087397
alexdamir <aamalyy@gmail.com>
2025-08-28 11:28:21 -0400

    Refactor `am_playwright_lesson2.py` by extracting login functionality into `login_linkmygear` function; introduce dynamic username and password inputs for reusability and using context.

--- python/am_playwright_lesson2.py
+++ python/am_playwright_lesson2.py
@@ -2,9 +2,8 @@ import uuid
 
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
 
+def login_linkmygear(browser, username:str, password:str):
     context = browser.new_context()
 
     context.tracing.start(name="trace", screenshots=True, snapshots=True)
@@ -13,9 +12,9 @@ with sync_playwright() as p:
     page.goto("https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com")
     page.wait_for_selector("//h5[.='Login to Your Account']", timeout=3000)
 
-    page.locator("//input[@name='username']").fill("email@email.com")
+    page.locator("//input[@name='username']").fill(username)
 
-    page.locator("//input[@name='password']").fill("1234")
+    page.locator("//input[@name='password']").fill(password)
 
     page.locator("//button").click()
 
@@ -24,5 +23,14 @@ with sync_playwright() as p:
     unique = str(uuid.uuid4())
     page.screenshot(path=f"C:\\Users\\aamal\\Pictures\\Screenshots\\Chromium_dev_linkmygear_{unique}.png")
     context.tracing.stop(path=f"C:\\Users\\aamal\\PycharmProjects\\Temp\\trace_{unique}.zip")
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    login_linkmygear(browser, username="7nxjno9lr@mozmail.com", password="r+WLLX9qwx^:>:3")
+
+    login_linkmygear(browser, username="email@email.com", password="1234")
+
 
     browser.close()
