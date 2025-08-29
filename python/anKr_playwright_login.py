e424bf96b917d8905315868d4f11ed240f0c95d1
AnKrylo <akrylova09876@gmail.com>
2025-08-29 17:23:36 -0400

    Update data

--- python/anKr_playwright_login.py
+++ python/anKr_playwright_login.py
@@ -1,98 +1,72 @@
-
-from colorama import Fore
 from playwright.sync_api import sync_playwright
 
 
-def navigate_to_page(page, url, verification_xpath):
-    page.goto(url)
-    page.wait_for_selector(verification_xpath, timeout=5000)
+browsers = ["chromium"]
 
-
-def fill_element(page, locator, value):
-    if value != "Skip":
-        page.locator(locator).fill(value)
-
-
-def login(page, username, password, button="//button"):
-    fill_element(page, username["locator"], username["value"])
-    fill_element(page, password["locator"], password["value"])
-    page.locator(button).click()
-
-
-login_data = [
-    [
-        "akr.autotest@gmail.com",
-        "12345",
-        "//h3[contains(text(), 'My device')]",
-        "Successful user login with valid credentials",
-    ],
-    [
-        "autotest@gmail.com",
-        "13579 ",
-        "//p[text()='Sorry, unrecognized username or password.']",
-        "Unsuccessful user login with invalid username and valid password",
-    ],
-    [
-        "akr.autotest@gmail.com",
-        "13579",
-        " //p[text()='Sorry, unrecognized username or password.'] ",
-        "Unsuccessful user login with invalid credentials",
-    ],
-    [
-        "autotest@gmail.com",
-        "12345",
-        " //p[text()='Sorry, unrecognized username or password.'] ",
-        "Unsuccessful user login with valid username and invalid password",
-    ],
-    [
-        "Skip",
-        "12345",
-        " //div[text()='Email is required']",
-        "Unsuccessful user login with empty username",
-    ],
-    [
-        "akr.autotest@gmail.com",
-        "Skip",
-        " //div[text()='Password is required']",
-        "Unsuccessful user login with empty password",
-    ],
-    [
-        "akr.autotest@gmail.com",
-        "12345  ",
-        " //div[text()='Password is required']",
-        "Unsuccessful user login with empty password",
-    ],
-]
-
-for one_login in login_data:
+for browser_type in browsers:
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-
-        print(one_login[3])
-
-        navigate_to_page(
-            page,
-            "https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com",
-            "//h5[.='Login to Your Account']",
-        )
-
-        login(
-            page,
-            {"locator": "//input[@name='username']", "value": one_login[0].strip()},
-            {"locator": "//input[@name='password']", "value": one_login[1].strip()},
-        )
-        try:
-            page.wait_for_selector(one_login[2].strip(), timeout=5000)
-
-        except Exception as error:
-            print(f"\t{Fore.RED}Element with XPath '{one_login[2].strip()}' does not exist: {error.name}{Fore.RESET}")
+        if browser_type == "chromium":
+            browser = p.chromium.launch(headless=False)
         else:
-            page.wait_for_timeout(5000)
-
+            browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com")
+        username = page.locator("//input[@name='username']")
+        username.click()
+        username.fill("akr.autotest@gmail.com")
+        password = page.locator("//input[@name='password']")
+        password.click()
+        password.type("12345", delay=500)
+        button = page.locator("//button")
+        button.click()
+        # page.screenshot(path=F"{browser_type}_dev_linkmygear.png")
         browser.close()
 
-
-
-
-
+#
+# Playwright lesson
+#
+# 
+# browsers = ["chromium"]
+#
+# for browser_type in browsers:
+#     with sync_playwright() as p:
+#         if browser_type == "chromium":
+#             browser = p.chromium.launch(headless=False)
+#         else:
+#             browser = p.firefox.launch(headless=False)
+#         context_dev = browser.new_context()
+#
+#         context_dev.tracing.start(name="trace", screenshots=True, snapshots=True)
+#
+#         page_dev = context_dev.new_page()
+#         page_dev.goto("https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com")
+#
+#         # context_app = browser.new_context()
+#         # page_app = context_app.new_page()
+#         # page_app.goto("https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com")
+#
+#         # page_dev.get_by_label("Sign in")
+#         # page_dev.get_by_text("Sign in")
+#         # page_dev.get_by_alt_text("Logo")
+#         #
+#         # page_dev.locator("div.dialog-off-canvas-main-canvas")
+#         #
+#         # page_dev.locator("//div[@class='dialog-off-canvas-main-canvas']")
+#
+#         # page.wait_for_selector("//button1", timeout=3000)
+#         # page.wait_for_timeout(2000)
+#         username = page_dev.locator("//input[@name='username']")
+#         username.fill("email")
+#         # username.type("email", delay=500)
+#         # username.click()
+#         password = page_dev.locator("//input[@name='password']")
+#         password.fill("1234")
+#         page_dev.keyboard.press("Enter")
+#         # password.click()
+#         # button = page.locator("//button")
+#         # button.click()
+#         # button.wait_for(state="visible")
+#         page_dev.screenshot(path=f"{browser_type}_dev_linkmygear.png")
+#
+#         context_dev.tracing.stop(path="../trace.zip")
+#         browser.close()
