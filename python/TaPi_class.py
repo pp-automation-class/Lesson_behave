ec1e919e714d644a2cc7c63f21bea0b720521c3d
TP <tlebe108@gmail.com>
2025-08-29 19:54:03 -0500

    Add Vegetables class with description and color check methods

---
+++ python/TaPi_class.py
@@ -0,0 +1,22 @@
+class Vegetables:
+    def __init__(self, name, color, root):
+        self.name = name
+        self.color = color
+        self.root = root
+
+    def describe (self):
+        root = "root" if self.root else "not root"
+        print (f"{self.name} is a {root} vegetable and its color is {self.color}.")
+
+    def is_orange(self):
+        return self.color == "orange"
+
+if __name__ == "__main__":
+    carrot = Vegetables ("carrot", "orange", True)
+    cucumber = Vegetables ("cucumber", "green", False)
+
+    carrot.describe()
+    cucumber.describe()
+
+    print(f"Is carrot an orange vegetable? {carrot.is_orange()}")
+    print(f"Is cucumber an orange vegetable? {cucumber.is_orange()}")
