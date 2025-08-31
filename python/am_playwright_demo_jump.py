84c851af6f2f355a6698e33d21947a6b58c063c3
alexdamir <aamalyy@gmail.com>
2025-08-31 16:49:52 -0400

    Add `am_test_demo_jump.py` for automated testing of demo jump functionality using Playwright

--- python/am_playwright_demo_jump.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-from pages.am_login_page import LoginPage
-
-with sync_playwright() as p:
-    page = p.chromium.launch(headless=False).new_context().new_page()
-    login_page = LoginPage(page)
-    login_page.navigate().login("7nxjno9lr@mozmail.com", "r+WLLX9qwx^:>:3")
-    assert login_page.verify_login_success()
