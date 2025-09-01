bfb3392c444abae7aef347b1dbe917797bcddbbb
Your Name <alena9tester@gmail.com>
2025-08-31 19:51:12 -0600

    лл

--- python/Лена.py
+++ python/Лена.py
@@ -10,4 +10,13 @@ def say_goodbye():
     print("Добрый вечер!")
 say_goodbye()
 
+def greet(name):
+    print (f"Hi, {name}!")
+greet("Lena")
+
 def say(name):
+    print (f"Hello, {name}!")
+say("Linda")
+
+def что_то(name):
+    print (f"Hi,")
