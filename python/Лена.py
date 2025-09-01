0f41ba9aef80b7ef484c1a6d7965d4c52e90d13e
Your Name <alena9tester@gmail.com>
2025-08-31 19:38:17 -0600

    Add `Лена.py` with basic greeting functions

---
+++ python/Лена.py
@@ -0,0 +1,13 @@
+def say_hello():
+    print("Hi there!")
+say_hello()
+
+def say_how_are_you():
+    print("How are you?")
+say_how_are_you()
+
+def say_goodbye():
+    print("Добрый вечер!")
+say_goodbye()
+
+def say(name):
