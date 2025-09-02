cb8e1496615d655c795c6d00b172905243ab8930
Lafera53 <78712561+Lafera53@users.noreply.github.com>
2025-09-01 19:19:40 -0500

    replicated and added notes

---
+++ python/lstark_test_news.py
@@ -0,0 +1,26 @@
+from login_page import LoginPage
+from devices_page import DevicesPage
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+def test_open_news():
+    # page.goto("dev.linkmygear.com")
+    # login_page = LoginPage(page)
+    # login_page.login("myemail", "mypassword")
+    # login_page.verify_login_success()
+    # the four lines above for login we can replace with service levels:
+    # loginservice.login("as user")
+    # devicesevice.create_device("123")
+    devices_page = DevicesPage(page)
+    devices_page.go_to_news()
+    devices_page.verify_on_news_page()
+
+
+
+
+
+
