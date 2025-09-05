2d5c38bcbc49ec1dc70e82ae5a5e442b50398d8a
Your Name <alena9tester@gmail.com>
2025-09-05 02:55:41 -0600

    Add newlines for better readability in Linda_classes.py

--- python/Linda_classes.py
+++ python/Linda_classes.py
@@ -1,33 +1,53 @@
+#
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# p1 = Person("Lena", 37)
+# print(p1.name, p1.age)
+#
+#
+# c1 = Person("Santa", 4)
+# print(c1.name)
+#
+#
+# class Cars:
+#     def __init__(self, years, car_type, doors):
+#         self.years = years
+#         self.car_type = car_type
+#         self.doors = doors
+#
+#     def describe(self):
+#
+#         print(f"Car:{self.car_type}, год выпуска: {self.years}, имеет дверей:{self.doors}")
+#
+#
+# c1 = Cars(2014, "Honda", 4)
+# print(c1.years, c1.car_type, c1.doors)
+# c2 = Cars(2020, "Audi", 2)
+# print(c2.years, c2.car_type, c2.doors)
+#
+# c1.describe()
+# c2.describe()
 
-class Person:
-    def __init__(self, name, age):
+
+class Dog:
+    def __init__(self, name, age, hair_color, breed):
         self.name = name
         self.age = age
+        self.hair_color = hair_color
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} говорит: Гав-гав!")
+        print("Когда радуется виляет хвостом")
+
+    def description(self):
+        print(f"{self.name} -- {self.age} года, порода {self.breed} имеет окрас {self.hair_color}")
 
 
-p1 = Person("Lena", 37)
-print(p1.name, p1.age)
-
-
-c1 = Person("Santa", 4)
-print(c1.name)
-
-
-class Cars:
-    def __init__(self, years, car_type, doors):
-        self.years = years
-        self.car_type = car_type
-        self.doors = doors
-
-    def describe(self):
-
-        print(f"Car:{self.car_type}, год выпуска: {self.years}, имеет дверей:{self.doors}")
-
-
-c1 = Cars(2014, "Honda", 4)
-print(c1.years, c1.car_type, c1.doors)
-c2 = Cars(2020, "Audi", 2)
-print(c2.years, c2.car_type, c2.doors)
-
-c1.describe()
-c2.describe()
+dog1 = Dog("Santa", 4, "ginger", "mini golden doodle")
+dog1.bark()
+dog1.description()
