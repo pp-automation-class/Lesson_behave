e56628b36979041b7d22ef567e998a76cd0a3859
Your Name <alena9tester@gmail.com>
2025-09-04 19:59:31 -0600

    Refactor `Linda_classes.py` to improve formatting and readability

--- python/Linda_classes.py
+++ python/Linda_classes.py
@@ -3,11 +3,15 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
 p1 = Person("Lena", 37)
 print(p1.name, p1.age)
 
+
 c1 = Person("Santa", 4)
-print (c1.name)
+print(c1.name)
+
 
 class Cars:
     def __init__(self, years, car_type, doors):
@@ -15,13 +19,10 @@ class Cars:
         self.car_type = car_type
         self.doors = doors
     def describe(self):
+
         print(f"Car:{self.car_type}, год выпуска: {self.years}, имеет дверей:{self.doors}")
-
-
-
 c1 = Cars(2014, "Honda", 4)
 print(c1.years, c1.car_type, c1.doors)
-
 c2 = Cars(2020, "Audi", 2)
 print(c2.years, c2.car_type, c2.doors)
 
